b7e0db8db92d1f2407f18add88553c4959dc8d6a
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2014-01-23 08:05:36 +0100

    Update the examples and comment on them

--- lectures/14-macros/02macrosClient.scala
+++ lectures/14-macros/02macrosClient.scala
@@ -16,23 +16,30 @@ object MacrosClient extends App {
   trace(plus1Macro(answer))
   trace(plus1(answer))
 
-  //
+  //Let us see the raw tree.
   traceRaw(plus1Macro(answer))
 
   val l = List(answer)
-  trace(l map (_ + 1) map (_ * 2))
 
   def f() = l map (_ + 1)
   def g(l2: List[Int]) = l2 map (_ * 2)
 
+  //A list expression
+  trace(l map (_ + 1) map (_ * 2))
+  //An expression which runs the same algorithm, but in a way which is already
+  //harder for a macro to see.
+  trace(g(f()))
 
-  inspectComprehension(someObj.g(f()))
+  //Let's try to pattern match on the comprehension and replace it to perform map fusion:
+  trace(inspectComprehension(l map (_ + 1) map (_ * 2)))
 
+  //Let's try on this alternate form - will not work.
+  trace(inspectComprehension(g(f())))
 
+  //Other examples of fusion
   trace(l flatMap (x => List(x + 1)) map (_ * 2))
   trace(l map (_ + 1) flatMap (x => List(x * 2)))
 
-  trace(inspectComprehension(l map (_ + 1) map (_ * 2)))
   trace(inspectComprehension(l flatMap (x => List(x + 1)) map (_ * 2)))
   trace(inspectComprehension(l map (_ + 1) flatMap (x => List(x * 2))))
 }
